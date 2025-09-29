2751cd52a95dc849ca9d25d1069c565ea53e211b
KmNeetuSingh <itisneetusingh@gmail.com>
2025-09-29 11:30:26 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,12 @@ const app = express();
 
 // Middleware
 app.use(cors());
+app.use(cors({
+    origin: 'https://finance-ai-tracker-rs62.vercel.app',
+    methods: ['GET','POST','PUT','DELETE'],
+    credentials: true 
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
